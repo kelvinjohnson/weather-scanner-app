983ba5ef466f039d795e3e2326a8748f86cd539e
Kelvin Johnson <kelvin.b.johnson@gmail.com>
2020-04-05 16:45:37 +0100

    added database to template

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+import requests
+import config
+from .models import City
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metrics&appid='
-    return render(request, 'index.html')
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
+    city = 'London'
+    cities = City.objects.all()
+
+    weather_info = []
+
+    for city in cities:
+
+        response = requests.get(url.format(city, config.api_key)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': response['main']['temp'],
+            'description': response['weather'][0]['description'],
+            'icon': response['weather'][0]['icon'],
+        }
+
+        weather_info.append(city_weather)
+
+    context = {'weather_info': weather_info}
+
+    return render(request, 'index.html', context)
 
